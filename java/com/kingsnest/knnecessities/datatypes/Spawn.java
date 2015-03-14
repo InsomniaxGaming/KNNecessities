a4d7d1134b768423b928377444d52f77e31c2e2d
Insomniac10102 <ins10102@gmail.com>
2015-03-13 22:53:00 -0700

    Initial spawn class

---
+++ java/com/kingsnest/knnecessities/datatypes/Spawn.java
@@ -0,0 +1,12 @@
+package com.kingsnest.knnecessities.datatypes;
+
+import java.util.UUID;
+
+public class Spawn extends Home{
+
+	public Spawn(Location loc, UUID ownerUUID) {
+		super(loc, ownerUUID);
+		//TODO set homename to spawn or something.
+	}
+
+}
